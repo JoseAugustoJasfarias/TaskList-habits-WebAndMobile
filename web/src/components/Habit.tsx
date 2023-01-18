fcc9da1a0333a9304da1ce887d0bc93aa8eae336
Jose Augusto <josejasfarias@gmail.com>
2023-01-18 14:43:01 -0300

    Iniciando Projeto Mobile e Web Lista de Tarefas

---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,10 @@
+
+interface HabitProps {
+  completed: number
+}
+
+export function Habit(props: HabitProps){
+  return (
+    <div className="bg-cyan-200 w-10 h-10  rounded m-2 flex items-center justify-center ">{props.completed}</div>
+  )
+}
